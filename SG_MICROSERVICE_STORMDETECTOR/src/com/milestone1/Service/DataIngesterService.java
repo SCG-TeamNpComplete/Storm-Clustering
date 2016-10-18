a4b06d15f879ca988ebb39d295d51dc871e241ad
Kedar <kedar.gn20@gmail.com>
2016-10-18 19:13:03 -0400

    storm clustering travis settings

--- SG_MICROSERVICE_STORMDETECTOR/src/com/milestone1/Service/DataIngesterService.java
+++
@@ -1,7 +0,0 @@
-package com.milestone1.Service;
-
-public class DataIngesterService {
-	public String returnBuiltURL(String date,int time,String NEXRADname ){
-		return "https://aws.amazon.com/noaa-big-data/nexrad/"+"/"+date+"/"+time+"/"+NEXRADname;
-	}
-}
--- scripts/install.sh
+++ scripts/install.sh
@@ -1,21 +1,4 @@
-echo 'Installing SG_MICROSERVICE_STORMDETECTOR' >> /var/log/tomcat.log
-cd '/home/ec2-user/SG_MICROSERVICE_STORMDETECTOR'
-mvn -e clean install >> /var/log/tomcat.log
-mv target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/
-echo 'Installing SG_MICROSERVICE_DATAINGESTOR' >> /var/log/tomcat.log
-cd '/home/ec2-user/SG_MICROSERVICE_DATAINGESTOR'
-mvn -e clean install >> /var/log/tomcat.log
-cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
-cd  /usr/local/tomcat7/apache-tomcat-7.0.72
-echo 'Installing SG_MICROSERVICE_STROMCLUSTERING' >> /var/log/tomcat.log
-cd '/home/ec2-user/SG_MICROSERVICE_STROMCLUSTERING'
-mvn -e clean install >> /var/log/tomcat.log
-cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
-cd  /usr/local/tomcat7/apache-tomcat-7.0.72
-echo 'Installing SG_MICROSERVICE_REGISTRY' >> /var/log/tomcat.log
-cd '/home/ec2-user/SG_MICROSERVICE_REGISTRY'
+sudo cd '/home/ec2-user/storm-clustering/SG_MICROSERVICE_STROMCLUSTERING'
 sudo mvn -e clean install >> /var/log/tomcat.log
-cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
-cd  /usr/local/tomcat7/apache-tomcat-7.0.72
-
-sudo sh ./bin/startup.sh >> /var/log/tomcat.log 2>&1 &
+sudo cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
+sudo sh /usr/local/tomcat7/apache-tomcat-7.0.72/bin/startup.sh >> /var/log/tomcat.log 2>&1 &
