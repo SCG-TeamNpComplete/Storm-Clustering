2c3066d5ffc3318fc31c046d2db2f1c8ee80bd88
Kedar <kedar.gn20@gmail.com>
2016-10-20 19:30:41 -0400

    removed sudo for cd

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
-sudo cd '/home/ec2-user/SG_MICROSERVICE_STROMCLUSTERING'
+cd '/home/ec2-user/SG_MICROSERVICE_STROMCLUSTERING'
 sudo mvn -e clean install >> /var/log/tomcat.log
 sudo cp target/*.war /usr/local/tomcat7/apache-tomcat-7.0.72/webapps/ >> /var/log/tomcat.log
 sudo sh /usr/local/tomcat7/apache-tomcat-7.0.72/bin/startup.sh >> /var/log/tomcat.log 2>&1 &
