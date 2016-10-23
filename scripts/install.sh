516bf0b843ff1df52b795fd8cb647282d22f562c
Kedar <kedar.gn20@gmail.com>
2016-10-23 05:54:38 -0400

    test env

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 echo 'starting installation process'
 cd '/home/ec2-user/docker'
 sudo docker login -e="kedar.gn20@gmail.com" -u="kedargn" -p="npcomplete"   #TODO : hide password
-sudo docker pull kedargn/stormcluster
-sudo docker run -d -p 8888:8080 --name stormcluster $(sudo docker images | grep kedargn/stormcluster | awk '{print $3}') >> ./log.txt
+sudo docker pull kedargn/stormclustertest
+sudo docker run -d -p 8888:8080 --name stormclustertest $(sudo docker images | grep kedargn/stormclustertest | awk '{print $3}') >> ./log.txt
