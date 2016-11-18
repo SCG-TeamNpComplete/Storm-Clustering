29ba56d4dad75d8932f5bafe15bb5f64e3e4d086
Kedar <kedar.gn20@gmail.com>
2016-11-18 04:04:55 -0500

    changed no. of instances #9

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,7 +4,7 @@ cd '/home/ec2-user/docker'
 sudo docker login -u="kedargn" -p="npcomplete"   #TODO : hide password
 sudo docker pull kedargn/stormcluster
 
-no_of_instances=3     #change this to set the number of instances
+no_of_instances=1     #change this to set the number of instances
 current_instance=1
 port=8888
 while [ $current_instance -le $no_of_instances ]
